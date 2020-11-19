751a467b437015206074e2c9b674c2b15dee7d2c
Tom Newby <me@tomnewby.net>
2020-11-19 12:29:04 +1000

    Pass exit code along

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,16 @@
 # Store stdout from the aws-cli call into a variable
 RESULT=$(aws "$@")
 
+# Capture the exit code and we'll use the same code at the end of this script
+# That means failures will be visible in the console
+EXITCODE=$?
+
 # Spit out the output for people reading logs
 echo $RESULT
 
 # Export the variable as an output result named "result"
 echo "::set-output name=result::$RESULT"
+
+echo "Exiting with code $EXITCODE"
+
+exit $EXITCODE
